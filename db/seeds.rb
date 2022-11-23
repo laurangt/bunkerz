b14f98dbe49d4353ae798e8eb58cd2d0a21ef2b5
Laura Nguyen Trong <laurangt@yahoo.com>
2022-11-23 15:50:23 +0100

    asdf

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,45 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-require "open-uri"
 
-puts "Cleanig DB"
-Bunker.destroy_all
-User.destroy_all
-
-puts "Creating Offers and Users"
-
-user1 = User.new
-user1.username = 'Sally'
-user1.email = 'user1@timepeek.com'
-user1.bio = 'Chef'
-user1.password = '123456'
-user1.password_confirmation = '123456'
-user1.encrypted_password = '123456'
-user1.save
+10.times do
+  Bunker.create!(
+    title: 'Innsbruck Bunker',
+    location: 'Innsbruck',
+    description: 'Nice',
+    price: 2000,
+    feature: 'Bathroom',
+    summary: 'Cooler',
+  )
+end
 
 
-bunker1 = Bunker.new
-bunker1.title = 'Inssbruck Bunker'
-bunker1.description = 'Nice'
-bunker1.price =  2000
-bunker1.feature = 'Bathroom'
-bunker1.summary = 'Cooler'
-bunker1.user_id = user1.id
-bunker1.save
+# require "open-uri"
+
+# puts "Cleanig DB"
+# Bunker.destroy_all
+# User.destroy_all
+
+# puts "Creating Offers and Users"
+
+# user1 = User.new
+# user1.username = 'Sally'
+# user1.email = 'user1@timepeek.com'
+# user1.bio = 'Chef'
+# user1.password = '123456'
+# user1.password_confirmation = '123456'
+# user1.encrypted_password = '123456'
+# user1.save
+
+
+# bunker1 = Bunker.new
+# bunker1.title = 'Inssbruck Bunker'
+# bunker1.description = 'Nice'
+# bunker1.price =  2000
+# bunker1.feature = 'Bathroom'
+# bunker1.summary = 'Cooler'
+# bunker1.user_id = user1.id
+# bunker1.save
 
 # new_bunker = Bunker.new(
 #   title: 'Inssbruck Bunker',
@@ -45,4 +58,3 @@ bunker1.save
 
 # new_bunker.save
 # puts new_bunker.title
-
