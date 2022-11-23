657045d987aed84ff403570dad1f6f6ea514a9f1
Maria Villalonga <mvillalongacdv@gmail.com>
2022-11-23 12:07:10 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_164324) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_110639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_164324) do
     t.integer "capacity"
     t.float "price"
     t.bigint "city_id", null: false
+    t.string "feature"
+    t.string "summary"
     t.index ["city_id"], name: "index_bunkers_on_city_id"
     t.index ["user_id"], name: "index_bunkers_on_user_id"
   end
@@ -67,6 +69,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_164324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "avatar_url"
+    t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
