68a7bc9a78dd7a1e455d7b49e24df74ffb43896c
Laura Nguyen Trong <laurangt@yahoo.com>
2022-11-23 16:33:45 +0100

    fake profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-10.times do
-  Bunker.create!(
-    title: 'Innsbruck Bunker',
-    location: 'Innsbruck',
-    description: 'Nice',
-    price: 2000,
-    feature: 'Bathroom',
-    summary: 'Cooler',
-  )
-end
 
-
-# require "open-uri"
+require "open-uri"
 
 # puts "Cleanig DB"
 # Bunker.destroy_all
@@ -27,24 +16,34 @@ end
 
 # puts "Creating Offers and Users"
 
-# user1 = User.new
-# user1.username = 'Sally'
-# user1.email = 'user1@timepeek.com'
-# user1.bio = 'Chef'
-# user1.password = '123456'
-# user1.password_confirmation = '123456'
-# user1.encrypted_password = '123456'
-# user1.save
+user1 = User.new
+user1.username = 'Sally'
+user1.email = 'user1@timepeek.com'
+user1.bio = 'Chef'
+user1.password = '123456'
+user1.password_confirmation = '123456'
+user1.encrypted_password = '123456'
+user1.save
 
 
-# bunker1 = Bunker.new
-# bunker1.title = 'Inssbruck Bunker'
-# bunker1.description = 'Nice'
-# bunker1.price =  2000
-# bunker1.feature = 'Bathroom'
-# bunker1.summary = 'Cooler'
-# bunker1.user_id = user1.id
-# bunker1.save
+bunker1 = Bunker.new
+bunker1.title = 'Inssbruck Bunker'
+bunker1.description = 'Nice'
+bunker1.price =  2000
+bunker1.feature = 'Bathroom'
+bunker1.summary = 'Cooler'
+bunker1.user_id = 2
+bunker1.save
+
+bunker1 = Bunker.new
+bunker1.title = 'Inssbruck Bunker'
+bunker1.description = 'Nice'
+bunker1.price =  2000
+bunker1.feature = 'Bathroom'
+bunker1.summary = 'Cooler'
+bunker1.user_id = 2
+bunker1.save
+
 
 # new_bunker = Bunker.new(
 #   title: 'Inssbruck Bunker',
